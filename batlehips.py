8af96b9723211b45e9c353f34d3a28a204cbc853
Reuben Taylor <q13rtaylor@wgsf.net>
2020-10-05 20:03:33 +0100

    Made grid properly

--- batlehips.py
+++ batlehips.py
@@ -1,4 +1,18 @@
-GridSize = 5
+GridSize = int(input("how big do you want the board?: "))
+
+ocean = []
 
 for i in range(1, GridSize + 1):
-    print("~" * GridSize)
+    ocean.append(["~"] * GridSize)
+
+for row in ocean:
+    print(" ".join(row))
+
+RowAnswer = int(input("please put your row answer here from 0 to grid size: "))
+ColumnAnswer = int(input("please put your column answer here as well from 0 to grid size: "))
+
+ocean[RowAnswer][ColumnAnswer] = "x"
+
+
+for row in ocean:
+    print(" ".join(row))
