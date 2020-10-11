dbbb569581a3249f5c33adbacdace7b84c12d53d
Reuben Taylor <q13rtaylor@wgsf.net>
2020-10-11 13:57:40 +0100

    added commentings on the battleship game lark

--- batlehips.py
+++ batlehips.py
@@ -1,18 +1,24 @@
+# takes the size of the grid that the user wants
 GridSize = int(input("how big do you want the board?: "))
 
+# makes an empty list that makes an ocean of coordinates
 ocean = []
 
+# making the board
 for i in range(1, GridSize + 1):
     ocean.append(["~"] * GridSize)
 
+# renders ocean thing to screen thing
 for row in ocean:
     print(" ".join(row))
 
+#takes in the coordinates for the user's guess
 RowAnswer = int(input("please put your row answer here from 0 to grid size: "))
 ColumnAnswer = int(input("please put your column answer here as well from 0 to grid size: "))
 
+#put's an 'x' for the user's grid coordinate
 ocean[RowAnswer][ColumnAnswer] = "x"
 
-
+# line 11
 for row in ocean:
     print(" ".join(row))
